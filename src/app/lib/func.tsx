785a0810fb36cb0c95d088c8681058bedc7df69e
Vib-UX <vibhav@instaraise.io>
2024-08-12 14:37:13 +0530

    fix: provider usdc balance base-sepolia

--- src/app/lib/func.tsx
+++ src/app/lib/func.tsx
@@ -15,9 +15,15 @@ const schemaUID =
   "0x20351f973fdec1478924c89dfa533d8f872defa108d9c3c6512267d7e7e5dbc2";
 const usdcAddress = "0x036CbD53842c5426634e7929541eC2318f3dCF7e"; // BaseSepolia USDC contract address
 
-export const getUSDCBalance = async (signer: any) => {
-  const userAddress = await signer.getAddress();
-  const usdcContract = new ethers.Contract(usdcAddress, usdcInterface, signer);
+export const getUSDCBalance = async (userAddress: string) => {
+  const provider = new ethers.providers.JsonRpcProvider(
+    "https://base-sepolia.g.alchemy.com/v2/7zVK4VTvAuz02lmtuEpPeorhM_Kg9PXz"
+  );
+  const usdcContract = new ethers.Contract(
+    usdcAddress,
+    usdcInterface,
+    provider
+  );
   const balance = await usdcContract.balanceOf(userAddress);
   return ethers.utils.formatUnits(balance, 6);
 };
